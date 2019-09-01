3ee8d51ac74b7f054b943f0007d0f42f21bcf95d
AdiRoH <53788783+AdiRoH@users.noreply.github.com>
2019-09-01 08:29:53 +0300

    Add files via upload

---
+++ writingShell_29082019/shell.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main()
+{
+	pid_t pid;
+	char path[]={"/home/adi/Desktop/OOP/writingShell_29082019/exe"};
+	char exe[]={"exe"};
+
+	pid=fork();
+	
+	if(pid==0)
+	{
+		execl(path, exe,(char*)NULL);
+	}
+
+	sleep(1);
+
+	return 0;
+}
